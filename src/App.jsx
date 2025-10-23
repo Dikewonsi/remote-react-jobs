8890813e71f642cafd84482f2313ee961ceb7bc6
Dikewonsi <91637528+Dikewonsi@users.noreply.github.com>
2025-10-23 10:43:37 +0100

    chore: get data on JobListings page from json-server

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import {
 import React from 'react'
 import MainLayout from './layouts/MainLayout';
 import HomePage from './pages/HomePage';
+import JobsPage from './pages/JobsPage';
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
     createRoutesFromElements(
       <Route path='/' element={<MainLayout />}>
         <Route index element={<HomePage />} />
+        <Route path='/jobs' element={<JobsPage />} />
       </Route>
     )
   )
