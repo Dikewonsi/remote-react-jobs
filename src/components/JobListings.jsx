3a88135f0a5b23a72ec2bda06aba968541151f1c
Dikewonsi <91637528+Dikewonsi@users.noreply.github.com>
2025-10-25 21:35:54 +0100

    feat: add search to Navbar, with updated URL params

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { useState, useEffect } from 'react'
 import Spinner from './Spinner'
 import JobListing from './JobListing'
+import { FaCircleXmark } from 'react-icons/fa6'
 
 const JobListings = ({ isHome = false }) => {
   const [jobs, setJobs] = useState([])
   const [loading, setLoading] = useState(true)
+  const location = useLocation();
+
+  // Here Joblisting reads the query in the URL 
+  const query = new URLSearchParams(location.search);
+  const searchTerm = query.get("search");
 
   useEffect (() => {
     const fetchJobs = async () => {
@@ -16,7 +22,17 @@ const JobListings = ({ isHome = false }) => {
       try {
         const res = await fetch(apiUrl)
         const data = await res.json()
-        setJobs(data)
+
+        if(searchTerm) {
+          const filtered = data.filter(
+            (job) =>
+              job.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+              job.company.name.toLowerCase().includes(searchTerm.toLowerCase())
+          );
+          setJobs(filtered);
+        } else {
+          setJobs(data)
+        }
       } catch (error) {
         console.error('Error fethcing Jobs', error)
       } finally {
@@ -25,7 +41,7 @@ const JobListings = ({ isHome = false }) => {
     }
 
     fetchJobs()
-  }, [])
+  }, [searchTerm]);
 
   return (
      <section className="bg-blue-50 px-4 py-10">
@@ -38,12 +54,21 @@ const JobListings = ({ isHome = false }) => {
           <div className="flex justify-center items-center min-h-[300px]">
             <Spinner loading={loading} />
           </div>
-        ) : (
+        ) : jobs.length > 0 ? (
           <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
             {jobs.map((job) => (
               <JobListing key={job.id} job={job} />
             ))}
           </div>
+        ) : (
+          <p className="text-center text-gray-600 text-lg mt-10">
+            ❌ No results found
+            {searchTerm && (
+              <>
+                {' '}for "<span className="font-semibold text-indigo-600">{searchTerm}</span>"
+              </>
+            )}
+          </p>
         )}
       </div>
     </section>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,54 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useNavigate, useLocation } from 'react-router-dom'
 import logo from '../assets/images/logo.png'
+import { FaSearch } from 'react-icons/fa'
+import { useState, useEffect } from 'react'
 
 
 const Navbar = () => {
+    
+    const [searchTerm, setSearchTerm] = useState('');
+    const [debouncedTerm, setDebouncedTerm] = useState('');
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    useEffect (() => {
+      const query = new URLSearchParams(location.search)
+      const termFromURL = query.get('search') || ''
+      setSearchTerm(termFromURL)
+    }, [location.search])
+
+
+    // 1. Debounce Logic using useEffect 
+    useEffect (() => {
+      const timer = setTimeout(() => {
+        setDebouncedTerm(searchTerm); // only update after 500ms of no typing
+      }, 500);
+
+      // Cleanup: cancel the timer if user types again before 500ms
+      return () => clearTimeout(timer);
+    }, [searchTerm]);
+
+    //2. Actual search logic runs only after debounce delay
+    useEffect (() => {
+      const trimmed = debouncedTerm.trim()
+
+      if (trimmed === '') {
+        //if search is cleared, go back to /jobs
+        if (location.pathname === '/jobs') {
+          navigate('/jobs')
+        }
+        return
+      } 
+
+        // Navigate to /jobs?search=value
+      navigate(`/jobs?search=${encodeURIComponent(trimmed)}`)
+    }, [debouncedTerm])
 
     const linkClass = ({ isActive }) =>
         isActive
         ? 'bg-black text-white hover:bg-gray-900 hover:text-white rounded-md px-3 py-2'
-        : 'text-white hover:bg-gray-900 hiver:text-white rounded-md px-3 py-2'
+        : 'text-white hover:bg-gray-900 hiver:text-white rounded-md px-3 py-2';
 
   return (
     <nav className="bg-indigo-700 border-b border-indigo-500">
@@ -45,6 +85,26 @@ const Navbar = () => {
                   className={linkClass}
                   >Add Job
                 </NavLink>
+
+                {/* Search Form */}
+                <form
+                  className="relative hidden sm:block"
+                >
+                  <input
+                    type="text" 
+                    placeholder="type here to search..."
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                    className="rounded-md py-1 px-3 focus:outline-none focus:ring-2 focus:ring-indigo-400 text-gray-900"
+                  />
+
+                  <button
+                    className="absolute right-0 top-0 mt-1 mr-2 text-gray-500 hover:text-gray-700"
+                  >
+                    <FaSearch />
+                  </button>
+
+                </form>
               </div>
             </div>
           </div>
